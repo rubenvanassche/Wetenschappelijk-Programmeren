a8b7b5bfc4c016af081f33125f2219f1c2546278
Ruben Van Assche <rubenvanassche@gmail.com>
2016-11-07 09:26:39 +0100

    Lab 2 and 3

---
+++ Lab 3/main.cpp
@@ -0,0 +1,107 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <gsl/gsl_errno.h>
+#include <gsl/gsl_spline.h>
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <fstream>
+#include <string>
+
+
+void writePointsFile(std::vector< std::pair<double, double> >& points, std::string filename){
+    std::ofstream file;
+    file.open(filename);
+
+    file << "#  X   Y" << std::endl;
+    for(auto i: points){
+        file << i.first << "   " << i.second << std::endl;
+    }
+
+    file.close();
+}
+
+void cubicSplineInterpolation(double x[], double y[], int size){
+    gsl_interp_accel *acc = gsl_interp_accel_alloc();
+    gsl_spline* spline = gsl_spline_alloc(gsl_interp_cspline_periodic, size);
+    gsl_spline_init(spline, x, y, size);
+
+    std::vector< std::pair<double, double> > points;
+
+    for(double xi = x[0];xi <= x[size-1];xi += 0.01){
+        double yi = gsl_spline_eval(spline, xi, acc);
+
+        auto pair = std::make_pair(xi,yi);
+        points.push_back(pair);
+    }
+
+    writePointsFile(points, "CubicSpline.dat");
+
+}
+
+void naturalSplineInterpolation(double x[], double y[], int size){
+    gsl_interp_accel *acc = gsl_interp_accel_alloc();
+    gsl_spline* spline = gsl_spline_alloc(gsl_interp_cspline, size);
+    gsl_spline_init(spline, x, y, size);
+
+    std::vector< std::pair<double, double> > points;
+
+    for(double xi = x[0];xi <= x[size-1];xi += 0.01){
+        double yi = gsl_spline_eval(spline, xi, acc);
+
+        auto pair = std::make_pair(xi,yi);
+        points.push_back(pair);
+    }
+
+    writePointsFile(points, "naturalSpline.dat");
+
+}
+
+void polynomialInterpolation(double x[], double y[], int size){
+    gsl_interp_accel *acc = gsl_interp_accel_alloc();
+    gsl_interp* interpolant = gsl_interp_alloc (gsl_interp_polynomial, size);
+    gsl_interp_init(interpolant, x, y ,size);
+
+    std::vector< std::pair<double, double> > points;
+
+    for(double xi = x[0];xi <= x[size-1];xi += 0.01){
+        double yi = gsl_interp_eval(interpolant, x, y, xi, acc);
+
+        auto pair = std::make_pair(xi,yi);
+        points.push_back(pair);
+    }
+
+    writePointsFile(points, "Polynomial.dat");
+}
+
+int main (void){
+    // Set COUT Precision
+    std::cout.precision(20);
+
+    std::cout << "Building Points..." << std::endl;
+
+    double x[7] = {0,23,37,54,74,88,130};
+    double y[7] = {2.8, 3.6, 4.4, 5.5, 6.4, 7.2, 8.3};
+    int size = 7;
+
+    std::vector< std::pair<double, double> > points;
+
+    for(int i = 0;i < 7;i++){
+        auto pair = std::make_pair(x[i],y[i]);
+        points.push_back(pair);
+
+        std::cout << "x: " << x[i] << ", y: " << y[i] << std::endl;
+    }
+
+    writePointsFile(points, "Points.dat");
+
+
+    std::cout << "Building Interpolants ..." << std::endl;
+    polynomialInterpolation(x, y, size);
+    naturalSplineInterpolation(x, y, size);
+    cubicSplineInterpolation(x, y, size);
+
+
+
+}
